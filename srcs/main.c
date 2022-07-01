e8fe5ce4dde3cd070c65dd56207f584017994d3c
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-07-01 15:13:56 +0900

    fix main.c and add error message

--- srcs/main.c
+++ srcs/main.c
@@ -1,25 +1,27 @@
 #include "board.h"
+#include "libft.h"
 
 int main(int argc, char** argv) {
-  t_board* board;
-  int input_fd;
-  switch (argc)
-  {
-  case 1:
-    input_fd = 1;
-    break;
-  case 2:
-    // input_fd = open_board_file(argv[1]);
-    break;
-  default:
-    // usage: ./alcu [board_file]
-    return 1;
-  }
-  // board = init_board(input_fd);
-  // start_game(board);
+	t_board*	board;
+	int			input_fd;
 
-  (void)board;
-  (void)input_fd;
-  (void)argv;
-  return 0;
+	switch (argc)
+	{
+	case 1:
+		input_fd = 1;
+		break;
+	case 2:
+		// input_fd = open_board_file(argv[1]);
+		break;
+	default:
+		ft_putendl_fd("usage: ./alcu [board_file]", STDERR_FILENO);
+		return 1;
+	}
+	// board = init_board(input_fd);
+	// start_game(board);
+
+	(void)board;
+	(void)input_fd;
+	(void)argv;
+	return 0;
 }
